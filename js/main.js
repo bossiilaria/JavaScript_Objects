7315e7f86e7476d37111328e5c5b0710ff5a478a
Ilaria Bossi <bossiilaria@users.noreply.github.com>
2015-10-18 16:43:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 var bob = new Object();
 bob.age = 56;
 bob.name = "Bob Smith";
@@ -17,3 +18,71 @@ alert(d.constructor);
 var n = new Number();
 alert(n.constructor);
 
+
+function Cat(age, color) {
+	this.age=age;
+	this.color=color;
+}
+function Dog(age, name, breed, size) {
+	this.age=age;
+	this.name = name;
+	this.breed =breed;
+	this.size=size;
+}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.species = "Homo Sapiens";
+}
+var sally = new Person();
+sally.name="Sally Bowles";
+sally.age=39;
+var holden = new Person();
+holden.name="Holden Caulfield";
+holden.age=16;
+
+
+function Rectangle(height, width) {
+	this.height = height;
+	this.width =width;
+	this.calcArea = function() {
+		return this.height * this.width;
+	};
+	this.calcPerimeter = function() {
+		return 2 * (this.width + this.height);
+	};
+}
+var rex = new Rectangle(7, 3);
+console.log(rex.calcArea());
+
+var x = 200;
+var y = 350;
+var ballWidth = 300;
+var ballHeight = 200;
+
+var draw = function() {
+	background(255, 206, 71);
+
+	fill(191, 0, 225);
+	ellipse(x, y, ballWidth, ballHeight);
+
+	x++;
+	y -= 2;
+	ballWidth *= 0.99;
+	ballHeight /=1.01;
+}
+console.log(x);
+*/
+
+var luba = {
+	name: "Luba",
+	status: "friend",
+	gender: "woman",
+	profession: "pianist",
+	passion: {
+		dance: "tango",
+		music: "piano"
+	}
+};
+alert(luba.name + " is my " + luba.status +". She is a very intelligent " + luba.gender + ". Luba is a " + luba.profession + 
+	". She is passionate about " + luba.passion.dance + " and " + luba.passion.music + ".");
